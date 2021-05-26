d52cb4c3bc116d3ce5266156c49826da08b93872
Ahmed Aly <ahmedaly22773@gmail.com>
2021-05-26 04:35:57 +0200

    initial commit

---
+++ help.h
@@ -0,0 +1,17 @@
+#ifndef _help_H
+#define _help_H
+
+float power( float base , float pow);
+
+void swap (int*  , int* );
+
+int IsPostive (int x);
+
+int factorial (int x);
+
+int IsPerfectSquare (int x);
+
+int SimpleCalculator (int x ,char op, int y);
+
+
+#endif
